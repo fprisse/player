14b0f2cd24767d0f85cc482f4e56d9b93f1e9d7c
Florent <florent@prisse.com>
2020-09-17 18:34:20 +0200

    hgf

--- player.js
+++ player.js
@@ -14,28 +14,24 @@ songs =
 'http://direct.fipradio.fr/live/fip-webradio2.mp3',
 'http://direct.fipradio.fr/live/fip-webradio3.mp3',
 'http://direct.fipradio.fr/live/fip-webradio4.mp3'
-'http://direct.fipradio.fr/live/fip-webradio5.mp3',
 './assets/music/dontstartnow.mp3']; // object storing paths for audio objects
 thumbnails = ['./assets/images/fip_radio.png',
 './assets/images/fip_rock.png',
 './assets/images/fip_jazz.png',
 './assets/images/fip_groove.png',
 './assets/images/fip_pop.png',
-'./assets/images/fip_electro.png'
 './assets/images/dontstartnow.png']; // object storing paths for album covers and backgrounds
 songArtists = ['FIP Stream 1',
 'FIP Stream 2',
 'FIP Stream 3',
 'FIP Stream 4',
 'FIP Stream 5',
-'FIP Stream 6',
 'Dua Lipa']; // object storing track artists
-songTitles = ['Radio',
+songTitles =
+['Radio',
 'Rock',
 'Jazz',
 'Groove',
-'Pop',
-'Electro',
 'Dont Start Now']; // object storing track titles
 
 // function where pp (play-pause) element changes based on playing boolean value - if play button clicked, change pp.src to pause button and call song.play() and vice versa.
